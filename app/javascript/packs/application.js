9a3debdc415749349e81e82efd9bcaeebb3c8c18
tnkmr08 <88134070+tnkmr08@users.noreply.github.com>
2021-09-16 18:01:59 +0900

    スライダープラグインの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,12 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+
+require("../slick")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +18,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
